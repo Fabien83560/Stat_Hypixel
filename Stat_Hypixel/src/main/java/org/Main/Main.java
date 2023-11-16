9213b0cba75f6b8633c70881477a4ad1fcb9aa46
MASSOL Evan <iitzwolfyytherenewal@gmail.com>
2023-11-16 11:41:17 +0100

    Add the base interface to the application

--- Stat_Hypixel/src/main/java/org/Main/Main.java
+++ Stat_Hypixel/src/main/java/org/Main/Main.java
@@ -2,10 +2,12 @@ package org.Main;
 
 import netscape.javascript.JSException;
 import org.Player.Player;
+import org.Window.Window;
 
 public class Main {
     public static void main(String[] args)
     {
         Player player = new Player("Fabien83560");
+        Window w = new Window();
     }
 }
---
+++ Stat_Hypixel/src/main/java/org/Window/Window.java
@@ -0,0 +1,50 @@
+package org.Window;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame{
+    private JButton quitButton;
+    private JPanel mainPanel;
+    private JButton compare2Players;
+    private JButton onePlayerButton;
+    private JPanel panelLeft;
+    private JScrollPane panelRight;
+    private JScrollPane friendListPanel;
+    private JTextField searchAPlayerHereTextField;
+    private JButton TODOButton;
+    private JButton bedWarsButton;
+    private JButton skyWarsButton;
+    private JButton TODOButton1;
+    private JButton TODOButton2;
+    private JList friendList;
+    private JLabel globalStatsLabel;
+    private JScrollPane globalStatsPanel;
+    private JLabel selectedModeLabel;
+    private JScrollPane selectedModePanel;
+
+    public Window(){
+        setTitle("Hypixel Statistics");
+        setSize(1400, 1000);
+        add(mainPanel);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        quitButton.setActionCommand("quit");
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getActionCommand().equals("quit")){
+                    if( JOptionPane.showConfirmDialog(null,
+                            "Do you want to quit ?",
+                            "Quit",
+                            JOptionPane.YES_NO_OPTION,
+                            JOptionPane.ERROR_MESSAGE) == JOptionPane.YES_OPTION )
+                    {
+                        System.exit(0);
+                    }
+                }
+            }
+        });
+    }
+}
