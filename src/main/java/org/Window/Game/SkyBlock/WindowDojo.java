8772c87dd5192862949706ad21a269226d88bce6
Fabien83560 <fabienortega.290604@gmail.com>
2023-12-16 22:50:08 +0100

    Add Dojo Interface

--- src/main/java/org/Window/Game/SkyBlock/WindowDojo.java
+++ src/main/java/org/Window/Game/SkyBlock/WindowDojo.java
@@ -6,9 +6,16 @@ import javax.swing.*;
 
 public class WindowDojo {
     private JPanel mainPanel;
+    private JLabel nameLabel;
+    private JLabel pointsLabel;
+    private JLabel rankLabel;
+    private JLabel timeLabel;
 
     public WindowDojo(Dojo dojo) {
-
+        nameLabel.setText(dojo.getName());
+        pointsLabel.setText("Points : " + dojo.getPoints());
+        rankLabel.setText("Rank : " + dojo.getRank());
+        timeLabel.setText("Time : " + dojo.getTime() + " sec");
     }
 
     public JPanel getMainPanel() {
